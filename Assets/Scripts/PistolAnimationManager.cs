9cad34fcfbf52ef46fcaf88176e559aceff3d3db
ConnectCode <omard2000@gmail.com>
2015-06-25 16:11:54 -0500

    Main Files Added

---
+++ Assets/Scripts/PistolAnimationManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistolAnimationManager : MonoBehaviour {
+	
+	public bool isWalking = false;
+	public bool isReloading = false;
+	
+	void Start() {
+		
+		GetComponent<Animation>().CrossFade("Idle");
+		
+	}
+	
+	void Update() {
+		
+		if(!GetComponent<Animation>().IsPlaying("Run")) {
+			
+			isWalking = false;
+			
+		}
+		
+		if(Input.GetKeyDown(KeyCode.R)) {
+			
+			isReloading= true;
+			isWalking = false;
+		}
+		
+		if(isReloading == true)
+		{
+			GetComponent<Animation>().CrossFade("Reload");
+		}
+		
+		if(Input.GetKeyDown(KeyCode.W)) {
+			
+			isWalking = true;
+			
+		}
+		
+		if(isWalking == true && isReloading == false) {
+			
+			GetComponent<Animation>().CrossFade("Run");
+			
+		}
+		else if(isWalking == false && isReloading == false) {
+			
+			GetComponent<Animation>().CrossFade("Idle");
+			
+		}
+		
+		if(Input.GetKeyUp(KeyCode.W)) {
+			
+			isWalking = false;
+			GetComponent<Animation>().CrossFade("Idle");
+			
+		}
+		
+		if(isWalking == false) {
+			
+			GetComponent<Animation>().CrossFade("Idle");
+			
+		}
+		
+	}
+	
+}
